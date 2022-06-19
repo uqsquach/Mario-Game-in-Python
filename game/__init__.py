5a212d9a7b24bf2c2a9dcc2badaa9883ea6a06aa
Jason Quach <84969568+uqsquach@users.noreply.github.com>
2022-06-20 00:59:57 +1000

    Update __init__.py

--- game/__init__.py
+++ game/__init__.py
@@ -1,14 +1,4 @@
-"""CSSE1001 Game Engine
 
-The CSSE1001 Game Engine has been developed by the tutors for the course
-to provide a base for the development of various different assignment games.
-
-Special thanks for Benjamin Martin and Paul Haley for the initial development
-of this game engine.
-
-The Game Engine depends on the pymunk python physics library and renders games
-using the tkinter library.
-"""
 
 __version__ = "1.1.0"
 __copyright__ = "The University of Queensland, 2019"
